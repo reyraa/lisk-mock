8546d87b7b2b21ce901191e419e9f82df813bb4d
reyraa <ali@lightcurve.io>
2017-10-17 10:51:47 +0200

    Add http/ws ports and os to peer object

--- src/models/peer.js
+++ src/models/peer.js
@@ -3,9 +3,11 @@ export default (i) => {
   const ip = [i%10, Math.floor(i/10), Math.floor(i/100)];
   return {
     ip: `100.${ip[0]}.${ip[1]}.${ip[2]}`,
-    port: 8000,
+    httpPort: 8000,
+    wsPort: 8000,
     version: 'v0.8.0',
     state: i%3,
+    os: 'debian',
     height: 123,
     broadhash: `258974416d58533227c6a3da1b6333f0541b06c65b41e45cf31926847a3db${i}ea`,
   }
